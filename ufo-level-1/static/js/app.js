4ba2624edf9004b80c68d4e920bb853a14968a65
User <c.j.dancewilson@outlook.com>
2021-08-13 08:58:59 +1000

    Confirmed current file working to load and filter table, it clears table before applying filtered values.

--- ufo-level-1/static/js/app.js
+++ ufo-level-1/static/js/app.js
@@ -4,6 +4,7 @@ var tableData = data;
 var tbody = d3.select("tbody");
 
 // YOUR CODE HERE!
+
 tableData.forEach(function(ufo) {
     // console.log(ufo);
     var row = tbody.append("tr");
@@ -16,7 +17,8 @@ tableData.forEach(function(ufo) {
     });
 });
 
-var form = d3.select("#form");
+
+var form = d3.select("form");
 var button = d3.select("#filter-btn");
 
 form.on("submit", searchDate);
@@ -30,4 +32,21 @@ function searchDate() {
 
     var inputValue = inputElement.property("value");
     console.log(inputValue);
+
+    var filteredData = tableData.filter(ufo => ufo.datetime === inputValue);
+    tbody.html("");
+    filteredData.forEach(function(ufo) {
+        
+        console.log(ufo);
+        var row = tbody.append("tr");
+
+        Object.entries(ufo).forEach(function([key, value]) {
+            console.log(key, value);
+
+            var cell = row.append("td");
+            cell.text(value);
+        });
+    });
+
 }
+
